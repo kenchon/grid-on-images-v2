e95db0610066ccf2c49162f7f609eaadf8acc106
kenchon <kenh5526@gmail.com>
2020-09-26 17:56:30 +0900

    feat: Add new component

---
+++ src/components/Canvas.tsx
@@ -0,0 +1,34 @@
+import React, { ReactElement } from 'react';
+import styled from 'styled-components';
+
+const CanvasContainer = styled.div`
+    position: relative;
+    canvas {
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
+`
+
+const ImgCanvas: React.FC = () => {
+    return (
+        <canvas></canvas>
+    )
+}
+
+const GridCanvas: React.FC = () => {
+    return (
+        <canvas></canvas>
+    )
+}
+
+const AppField: React.FC = () => {
+    return(
+        <CanvasContainer>
+            <ImgCanvas />
+            <GridCanvas />
+        </CanvasContainer>
+    )
+}
+
+export default AppField;
