1b4ed29566b2d28811bbdccfe807dad3eeb7bfd2
kenchon <kenh5526@gmail.com>
2020-09-27 23:36:32 +0900

    feat: Add Slider component

---
+++ src/components/Slider.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Slider: React.FC = () => {
+    return (
+        <div>
+          <span>➖</span>
+          <input type="range"/>
+          <span>➕</span>
+        </div>
+    )
+}
+
+export default Slider;
