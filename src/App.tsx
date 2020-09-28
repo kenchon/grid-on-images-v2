4a42ee7ff55a4f5de8762eb92f0c917b892efa78
kenchon <kenh5526@gmail.com>
2020-09-28 23:22:38 +0900

    feat: Add BodyContainer component to make space under Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import CanvasBoard from 'components/Canvas'
 import Header from 'components/Header'
+import Manipulator from 'components/Manipulator'
+import BodyContainer from 'components/BodyContainer'
 
 // Grid-on-Images のベースコンポーネント
 const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
-      <CanvasBoard />
+      <BodyContainer>
+        <Manipulator />
+        <CanvasBoard />
+      </BodyContainer>
     </div>
   );
 }
---
+++ src/components/BodyContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const BodyContainer: React.FC = styled.div`
+    margin: 200px 20px 0 30px;
+    width: 100%;
+`
+
+export default BodyContainer;
