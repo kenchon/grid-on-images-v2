f1049cd2bf5f20d46780344dcc1e2018dbb52fef
kenchon <kenh5526@gmail.com>
2020-09-29 21:35:33 +0900

    style: Update style of the <body/>

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,14 @@ import React from 'react';
 import CanvasBoard from 'components/Canvas'
 import Header from 'components/Header'
 import Manipulator from 'components/Manipulator'
-import BodyContainer from 'components/BodyContainer'
 
 // Grid-on-Images のベースコンポーネント
 const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
-      <BodyContainer>
-        <Manipulator />
-        <CanvasBoard />
-      </BodyContainer>
+      <Manipulator />
+      <CanvasBoard />
     </div>
   );
 }
--- src/components/BodyContainer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styled from 'styled-components';
-
-const BodyContainer: React.FC = styled.div`
-    margin: 200px 20px 0 30px;
-    width: 100%;
-`
-
-export default BodyContainer;
